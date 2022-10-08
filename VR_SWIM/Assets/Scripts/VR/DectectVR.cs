2a60ea6917b230b68890fcf8fed0820b2d45a912
Quy Nguyen Duc <quy.nguyen-duc@ubisoft.com>
2022-10-08 21:03:17 +0700

    add static character

--- VR_SWIM/Assets/Scripts/VR/DectectVR.cs
+++ VR_SWIM/Assets/Scripts/VR/DectectVR.cs
@@ -6,9 +6,8 @@ using UnityEngine.XR.Management;
 public class DectectVR : MonoBehaviour
 {
     public bool noVR;
-    public GameObject xrOrigin;
-    public GameObject desktopCharacter;
-    public GameObject MockHMD;
+    public GameObject[] desktopCharacter;
+    public GameObject[] vrGameObject;
     public static DectectVR instancne;
     public bool isVR;
     private void Awake()
@@ -33,23 +32,29 @@ public class DectectVR : MonoBehaviour
         if (xrLoader != null)
         {
             Debug.Log("Device detected" + xrLoader.name);
-            if(xrLoader.name.IndexOf("Mock HMD")!=-1)
-            {
-                Debug.Log("Using FAKE controller - MOCK HMD");
-                MockHMD.SetActive(true);
-            }
-            else
-            {
-                MockHMD.SetActive(false);
-            }
+            //if(xrLoader.name.IndexOf("Mock HMD")!=-1)
+            //{
+            //    Debug.Log("Using FAKE controller - MOCK HMD");
+            //    MockHMD.SetActive(true);
+            //}
+            //else
+            //{
+            //    MockHMD.SetActive(false);
+            //}
         }
         else
         {
             Debug.Log("No VR Device detect");
         }
         isVR = xrLoader != null && !noVR;
-        //xrOrigin.SetActive(isVR);
-        desktopCharacter.SetActive(!isVR);
+        foreach(var ob  in vrGameObject)
+        {
+            ob.SetActive(isVR);
+        }
+        foreach (var ob  in desktopCharacter)
+        {
+            ob.SetActive(!isVR);
+        }
     }
 
 }
