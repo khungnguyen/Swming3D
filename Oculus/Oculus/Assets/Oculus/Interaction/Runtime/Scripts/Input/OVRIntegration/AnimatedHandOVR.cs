ed0c95b636c0a46bbf9ff732296dcd7229eb05cd
Quy Nguyen Duc <quyq.ngd@gmail.com>
2022-11-05 11:46:56 +0700

    ! Use only one pose for hand

--- Oculus/Oculus/Assets/Oculus/Interaction/Runtime/Scripts/Input/OVRIntegration/AnimatedHandOVR.cs
+++ Oculus/Oculus/Assets/Oculus/Interaction/Runtime/Scripts/Input/OVRIntegration/AnimatedHandOVR.cs
@@ -36,21 +36,20 @@ namespace Oculus.Interaction.Input
 
         public const float INPUT_RATE_CHANGE = 20.0f;
 
-        [SerializeField]
-        private OVRInput.Controller _controller = OVRInput.Controller.None;
-        [SerializeField]
-        private Animator _animator = null;
+
+        public OVRInput.Controller _controller = OVRInput.Controller.None;
+        public Animator _animator = null;
         [SerializeField]
         private AllowThumbUp _allowThumbUp = AllowThumbUp.TriggerAndGripRequired;
 
-        private int _animLayerIndexThumb = -1;
-        private int _animLayerIndexPoint = -1;
-        private int _animParamIndexFlex = -1;
+        protected int _animLayerIndexThumb = -1;
+        protected int _animLayerIndexPoint = -1;
+        protected int _animParamIndexFlex = -1;
 
-        private bool _isPointing = false;
-        private bool _isGivingThumbsUp = false;
-        private float _pointBlend = 0.0f;
-        private float _thumbsUpBlend = 0.0f;
+        protected bool _isPointing = false;
+        protected bool _isGivingThumbsUp = false;
+        protected float _pointBlend = 0.0f;
+        protected float _thumbsUpBlend = 0.0f;
 
         private const float TRIGGER_MAX = 0.95f;
 
@@ -71,7 +70,7 @@ namespace Oculus.Interaction.Input
             UpdateAnimStates();
         }
 
-        private void UpdateCapTouchStates()
+        protected virtual void UpdateCapTouchStates()
         {
             _isPointing = !OVRInput.Get(OVRInput.NearTouch.PrimaryIndexTrigger, _controller)
                && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, _controller) == 0f;
@@ -91,6 +90,7 @@ namespace Oculus.Interaction.Input
                 && !OVRInput.Get(OVRInput.Button.PrimaryThumbstick, _controller)
                 && OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, _controller).magnitude == 0
                 && triggerThumbsUp;
+
         }
 
         /// <summary>
@@ -100,14 +100,14 @@ namespace Oculus.Interaction.Input
         /// <param name="isDown">Direction of the animation</param>
         /// <param name="value">Value to change</param>
         /// <returns>The input value increased or decreased at a fixed rate</returns>
-        private float InputValueRateChange(bool isDown, float value)
+        protected float InputValueRateChange(bool isDown, float value)
         {
             float rateDelta = Time.deltaTime * INPUT_RATE_CHANGE;
             float sign = isDown ? 1.0f : -1.0f;
             return Mathf.Clamp01(value + rateDelta * sign);
         }
 
-        private void UpdateAnimStates()
+        protected virtual void UpdateAnimStates()
         {
             // Flex
             // blend between open hand and fully closed fist
---
+++ Oculus/Oculus/Assets/Scripts/VR/HandVisual/AnimateHandOVRCusomtize.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Interaction.Input;
+using UnityEngine;
+
+public class AnimateHandOVRCusomtize : AnimatedHandOVR
+{
+    /**
+    * Force to disable thumb and pointin
+    */
+    protected override void UpdateCapTouchStates()
+    {
+        _isGivingThumbsUp = false;
+        _isPointing = false;
+        Debug.Log("XXXXXXXXXXXXXXXXXXXXXXXXX");
+    }
+    /**
+    * Change to hold for both grab and index button
+    */
+     protected override void UpdateAnimStates()
+    {
+        // Flex
+        // blend between open hand and fully closed fist
+        float flex = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, _controller);
+        float pinch = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, _controller);
+        float choice = Mathf.Max(flex,pinch);
+        _animator.SetFloat(_animParamIndexFlex, choice);
+        // Point
+        _animator.SetLayerWeight(_animLayerIndexPoint, _pointBlend);
+
+        // Thumbs up
+        _animator.SetLayerWeight(_animLayerIndexThumb, _thumbsUpBlend);
+
+     
+    }
+}
